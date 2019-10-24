27919396c5a83a61ea606821e7c15408985a8a88
ShilpaNidoniGit <shilpa_nidoni@hotmail.com>
2019-10-24 07:47:47 +0530

    Update CardService.java

--- src/main/java/com/springboot/mongodb/restapi/service/CardService.java
+++ src/main/java/com/springboot/mongodb/restapi/service/CardService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springboot.mongodb.restapi.model.Card;
 import com.springboot.mongodb.restapi.repository.CardRepository;
@@ -14,6 +15,7 @@ public class CardService {
 	@Autowired
 	private CardRepository repository;
 	
+	@Transactional
 	public Card addCard(Card card){
 		return repository.save(card);
 	}
